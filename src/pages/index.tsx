34eef78ab817d95bcf886122156fd1c8d1ab5ed3
Marcus Furletti <136692042+marcusfurletti@users.noreply.github.com>
2023-11-17 14:05:38 +0000

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,13 +17,47 @@ const index: React.FC = () => {
     { id: 8, name: "USD" },
     { id: 9, name: "RUB" },
   ]);
+  const [selectedCurrency, setSelectedCurrency] = useState<Item[]>([]);
+
+  const handleClick = (item: Item) => {
+    const isSelected = selectedCurrency.some(
+      (currency) => currency.id === item.id
+    );
+
+    if (isSelected) {
+      setSelectedCurrency((prevSelectedItems) =>
+        prevSelectedItems.filter((selectedItem) => selectedItem.id !== item.id)
+      );
+    } else {
+      setSelectedCurrency((prevSelectedItems) => [...prevSelectedItems, item]);
+    }
+  };
 
   return (
     <div className="hero">
       <div className="container">
+        <h2 className="title">Seleceted Currencies</h2>
+        <div className="selectedCurrencyList">
+          {selectedCurrency.map((currency) => (
+            <div
+              className="currencyItem"
+              key={currency.id}
+              onClick={() => handleClick(currency)}
+            >
+              {currency.name}
+            </div>
+          ))}
+        </div>
+        <h2 className="title">Currency List</h2>
         <div className="currencyList">
           {currencyList.map((currency) => (
-            <div key={currency.id}>{currency.name}</div>
+            <div
+              className="currencyItem"
+              key={currency.id}
+              onClick={() => handleClick(currency)}
+            >
+              {currency.name}
+            </div>
           ))}
         </div>
       </div>
